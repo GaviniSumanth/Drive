3c1bf8fa6ff8215bef53a387350ad25daf435087
GaviniSumanth <sumanth_edu@outlook.com>
2023-09-03 15:22:40 +0530

    feat: add page and layout transitions

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    pageTransition: { name: 'page', mode: 'out-in' },
+    layoutTransition: { name: 'layout', mode: 'out-in' }
+  },
   css: ['@fortawesome/fontawesome-svg-core/styles.css', '~/assets/css/app.css'],
   postcss: {
     plugins: {
