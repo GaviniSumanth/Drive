12891508efa53833dc3f8204069175d637234cc0
GaviniSumanth <sumanth_edu@outlook.com>
2023-09-03 14:08:47 +0530

    feat: add welcome page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-const route = useRoute()
 useSeoMeta({
   title: '',
   description: '',
@@ -24,11 +23,33 @@ useHead({
 </script>
 
 <template>
-  <div class="flex flex-col">
-    <h1>Nuxt Routing set up successfully!</h1>
-    <p>Current route: {{ route.path }}</p>
-    <a href="https://nuxt.com/docs/getting-started/routing" target="_blank"
-      >Learn more about Nuxt Routing</a
-    >
-  </div>
+  <section class="flex min-h-screen items-center justify-center bg-white py-24">
+    <div class="mx-auto max-w-[43rem]">
+      <div class="text-center">
+        <p class="text-lg font-medium leading-8 text-indigo-600/95">
+          Introducing a new Storage Service
+        </p>
+        <h1
+          class="mt-3 text-[3.5rem] font-bold leading-[4rem] tracking-tight text-black"
+        >
+          Cloudy
+        </h1>
+        <p class="mt-3 text-lg leading-relaxed text-slate-400">Cloud Storage</p>
+      </div>
+
+      <div class="mt-6 flex items-center justify-center gap-4">
+        <NuxtLink
+          href="signin"
+          class="transform rounded-md bg-indigo-600/95 px-5 py-3 font-medium text-white transition-colors hover:bg-indigo-700"
+          >Get started for free</NuxtLink
+        >
+        <NuxtLink
+          href="signup"
+          class="transform rounded-md border border-slate-200 px-5 py-3 font-medium text-slate-900 transition-colors hover:bg-slate-50"
+        >
+          Sign in
+        </NuxtLink>
+      </div>
+    </div>
+  </section>
 </template>
