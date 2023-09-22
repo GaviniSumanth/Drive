9a6d84672f04b3e31f899e073fecf14a6a8b3988
GaviniSumanth <sumanth_edu@outlook.com>
2023-09-22 23:39:02 +0530

    feat: add card component template

---
+++ components/Card.vue
@@ -0,0 +1,17 @@
+<script setup></script>
+<template>
+  <div class="card">
+    <div
+      class="flex items-center justify-center bg-gradient-to-br from-indigo-500 to-indigo-800"
+    >
+      <button
+        class="m-5 max-w-xs overflow-hidden rounded-3xl bg-white text-center font-semibold shadow-lg"
+      >
+        <img
+          src="https://images.unsplash.com/photo-1682687982029-edb9aecf5f89?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2071&q=80"
+        />
+        <pre>file name</pre>
+      </button>
+    </div>
+  </div>
+</template>
